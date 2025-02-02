2ebad9b101d316cfea0c43fae99cf30b66ce55f5
Siyun Liang <siyun.liang@tum.de>
2025-02-02 22:47:14 +0100

    fix training

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -122,7 +122,7 @@ class GaussianModel:
             denom,
             opt_dict, 
             self.spatial_lr_scale) = model_args
-            if not training_args.include_feature: # 如果是以原始gs为初始化来训练feature的话，就不需要restore optimizer
+            if mode == 'train' and not training_args.include_feature: # 如果是以原始gs为初始化来训练feature的话，就不需要restore optimizer
                 self.optimizer.load_state_dict(opt_dict)
         
         if mode == 'train':
